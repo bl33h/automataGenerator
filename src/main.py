09275befada30a3a425602bd49507c167ab2f7f0
bl33h <per21371@uvg.edu.gt>
2023-09-27 14:38:23 -0600

    2/4 generated .txt files

--- src/main.py
+++ src/main.py
@@ -9,10 +9,16 @@ from regexToPostfix import regexToPostfix
 from regex2afn import Regex2AFNConverter
 from regex2afn import *
 from afn2afd import *
+import sys
+
+# Define los nombres de los archivos para cada resultado
+nfa_output_file = "nfa_output.txt"
+postfix_output_file = "postfix_output.txt"
+afd_output_file = "afd_output.txt"
 
 # alphabet and regex expression
 alphabet = "abce*+10"  # modify this according to your needs
-epsilon = 'e'
+epsilon = 'ε'  # Utiliza el carácter epsilon directamente
 
 print("\n* Project 1 - Automata Generator *")
 # prompt the user to enter a regular expression
@@ -21,55 +27,46 @@ expression = input("enter the regular expression: ")
 exit = True
 
 while exit:
-    option = input("\n--------------------\nSelect an option:\n (1)Regex to postfix\n (2)Regex to AFN\n (3)From AFN to AFD\n (4)Min AFD\n (5)Simulation of AFN, AFD and min AFD\n (6)Exit\n-------------------- \n>>> ")
+    option = input("\n--------------------\nSelect an option:\n (1)Regex to postfix\n (2)Regex to AFN\n (3)From AFN to AFD\n (4)Min AFD\n (5)Simulation of AFN, AFD, and min AFD\n (6)Exit\n-------------------- \n>>> ")
 
     if option == "1":
         # Convert the infix regular expression to postfix
-        postfix_expression = regexToPostfix(alphabet, expression, "e")
-        # print results
-        print("Postfix expression:", postfix_expression.getResult())
+        postfix_expression = regexToPostfix(alphabet, expression, epsilon)
+        # Print results
+        result = postfix_expression.getResult().replace('\u03b5', 'epsilon')
+        print("Postfix expression:", result)
+
+        # Guarda el resultado en el archivo
+        with open(postfix_output_file, "w", encoding="utf-8") as file:
+            file.write(result)
+            print("Result written to", postfix_output_file)
 
     if option == "2":
         postfix_expression = regexToPostfix(alphabet, expression, epsilon).getResult()
-        converter = Regex2AFNConverter("ε")
+        converter = Regex2AFNConverter(epsilon)
         nfa = converter.convert2NFA(postfix_expression)
-        converter.print_nfa(nfa)
+        
+        # Redirige la salida estándar al archivo nfa_output_file
+        with open(nfa_output_file, "w", encoding="utf-8") as file:
+            sys.stdout = file
+            converter.print_nfa(nfa)
+            sys.stdout = sys.__stdout__  # Restaura la salida estándar
+            
+            print("Result written to", nfa_output_file)
 
     if option == "3":
-         print_afd_results()
-        #postfix_expression = regexToPostfix(alphabet, expression, epsilon).getResult()
-        #converter = Regex2AFNConverter(epsilon, concat_operator="^")
-        #afn = converter.convert2NFA(postfix_expression)
-        #symbols = afn[0]
+        afd_results = print_afd_results()
         
-        # Modifica nfa_states para convertirlo en un diccionario
-        #nfa_states = {}
-        #for i, state_transitions in enumerate(afn[1]):
-            #nfa_states[i] = state_transitions
+        # Guarda los resultados en el archivo
+        with open(afd_output_file, "w", encoding="utf-8") as file:
+            file.write(afd_results)
+            print("Result written to", afd_output_file)
 
-        #nfa_start = afn[2]
-        #nfa_end = afn[3]
-
-        #print("Inputs:", symbols)
-        #print("AFN transitions:", nfa_states)
-
-        # Convertir el AFN a AFD utilizando AFN2AFDConverter
-        #afd_converter = AFN2AFDConverter(epsilon)
-        #afd_result = afd_converter.convert2DFA(symbols, nfa_states, nfa_start, [nfa_end])
-
-        # Mostrar los componentes del AFD resultante
-        #print("\nAFD Components:")
-        #print("Estados:", afd_result["Estados"])
-        #print("Simbolos:", afd_result["Simbolos"])
-        #print("Inicio:", afd_result["Inicio"])
-        #print("Aceptacion:", afd_result["Aceptacion"])
-        #print("Transiciones:", afd_result["Transiciones"])
-    
     if option == "4":
         print("")
     
     if option == "5":
-        print("")
-    
+        print("Redirecting output to text files.")
+
     if option == "6":
         exit = False
